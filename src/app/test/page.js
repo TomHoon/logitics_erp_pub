01f1225f76c462d7430b65bb60c52decb3a4c728
soonat <soonasama2@gmail.com>
2026-05-18 21:38:07 +0900

    feat: 프로젝트 셋팅

---
+++ src/app/test/page.js
@@ -0,0 +1,5 @@
+import Nav from '@/component/Nav';
+
+export default function page() {
+    return <Nav/>
+}
---
+++ src/component/Nav.jsx
@@ -0,0 +1,37 @@
+import style from './Nav.module.css'
+
+export default function Nav({initial = '홍', fullName = '홍길동'}) {
+    return (
+        <div className={style.container}>
+        
+            <div className={style.left}>
+                <div className={style.left1}>
+                    <img src="/briefcase.png"/>
+                    <span>인사관리시스템</span>
+                </div>
+
+                <div className={style.left2}>
+                    <ul className={style.navMenu}>
+                        <li className={style.active}>인사관리</li>
+                        <li>근태관리</li>
+                        <li>급여관리</li>
+                        <li>일용직관리</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className={style.right}>
+                <img src="/Bell.png" className={`${style.imgIcon} ${style.bellIcon}`}/>
+                <div className={style.nameWrapper}>
+                    <span className={style.circleInitial}>홍</span>
+                    <span className={style.fullName}>홍길동</span>
+                </div>
+                <div className={style.departWrapper}>
+                    <span>인사팀</span>
+                    <img src="/logout.png" className={style.imgIcon}/>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
