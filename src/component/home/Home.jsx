cf3d285c589434c0e9f068cc98d097ea8c7c3728
tomhoon <gnsdl9108@naver.com>
2026-06-18 05:52:35 +0900

    fix: 로그인유지

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-import c from "@/component/home/Home.module.css";
-import Nav from "@/component/common/Nav";
+import c from '@/component/home/Home.module.css';
+import Nav from '@/component/common/Nav';
 import {
 	BanknoteArrowUp,
 	Briefcase,
@@ -17,139 +17,160 @@ import {
 	Mail,
 	ShieldCheck,
 	UserPen,
-	Users
-} from "lucide-react";
-import {Checkbox} from "@/components/ui/checkbox";
-import CInputCustom from "@/component/common/element/CInputCustom";
-import {useState} from "react";
-import baseApi from "@/common/api/baseApi";
-import {useRouter} from "next/navigation";
+	Users,
+} from 'lucide-react';
+import { Checkbox } from '@/components/ui/checkbox';
+import CInputCustom from '@/component/common/element/CInputCustom';
+import { useEffect, useState } from 'react';
+import baseApi from '@/common/api/baseApi';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-	
+
 	const [loginUserInfo, setLoginUserInfo] = useState({});
-	
+	const [isSaveLogin, setIsSaveLogin] = useState(false);
+
 	const router = useRouter();
-	
+
 	const goLogin = async () => {
 		try {
-			const res = await baseApi.post("/api/v1/employees/login", {...loginUserInfo});
-			
+			const res = await baseApi.post('/api/v1/employees/login', { ...loginUserInfo });
+
 			if (res.data.data.accessToken) {
-				localStorage.setItem("accessToken", res.data.data.accessToken);
-				localStorage.setItem("user", JSON.stringify(res.data.data));
-				router.push("/info/register");
+				localStorage.setItem('accessToken', res.data.data.accessToken);
+				localStorage.setItem('user', JSON.stringify(res.data.data));
+
+				if (isSaveLogin) {
+					localStorage.setItem('loginInfo', JSON.stringify(loginUserInfo));
+				}
+
+				router.push('/info/register');
 				return;
 			}
-			
-			alert("로그인 실패");
-			
+
+			alert('로그인 실패');
+
 		} catch (e) {
-			alert("네트워크 통신에 실패하였습니다.")
-			
+			alert('네트워크 통신에 실패하였습니다.');
+
 		}
-		
-		
-	}
-	
-	
+
+
+	};
+
+	useEffect(() => {
+		const jsonIsSaveLogin = localStorage.getItem('isSaveLogin');
+		const flagOfSaveLogin = JSON.parse(jsonIsSaveLogin);
+		if (flagOfSaveLogin) {
+
+			const jsonLoginInfo = localStorage.getItem('loginInfo');
+			const savedLoginInfo = JSON.parse(jsonLoginInfo);
+
+			setLoginUserInfo({
+				email: savedLoginInfo.email,
+				password: savedLoginInfo.password,
+			});
+		}
+	}, []);
+
+
 	const renderJoin = () => {
 		return (
 			<div className={`${c.joinArea} ${c.stagger}`}>
-				
+
 				<div className={c.joinWrapper}>
 					<section className={c.titleSection}>
 						<p>회원가입</p>
 						<span>계정을 만들어 인사관리를 시작하세요</span>
 					</section>
-					
+
 					<section className={c.joinFormSection}>
 						<CInputCustom
-							labelName='성'
-							placeholder='성'
+							labelName="성"
+							placeholder="성"
 						/>
-						
+
 						<CInputCustom
-							labelName='이름'
-							placeholder='이름'
+							labelName="이름"
+							placeholder="이름"
 						/>
-						
+
 						<CInputCustom
-							beforeIcon={<IdCard color='#9CA3AF'/>}
-							labelName='사번'
-							placeholder='EMP-001'
+							beforeIcon={<IdCard color="#9CA3AF" />}
+							labelName="사번"
+							placeholder="EMP-001"
 						/>
-						
+
 						<CInputCustom
-							beforeIcon={<Building2 color='#9CA3AF'/>}
-							labelName='부서'
-							placeholder='소속 부서 선택'
+							beforeIcon={<Building2 color="#9CA3AF" />}
+							labelName="부서"
+							placeholder="소속 부서 선택"
 						/>
-						
+
 						<CInputCustom
-							beforeIcon={<Briefcase color='#9CA3AF'/>}
-							labelName='직급'
-							placeholder='직급선택'
+							beforeIcon={<Briefcase color="#9CA3AF" />}
+							labelName="직급"
+							placeholder="직급선택"
 						/>
-						
+
 						<CInputCustom
-							beforeIcon={<Mail color='#9CA3AF'/>}
-							labelName='회사 이메일'
-							placeholder='company@example.com'
+							beforeIcon={<Mail color="#9CA3AF" />}
+							labelName="회사 이메일"
+							placeholder="company@example.com"
 						/>
-						
+
 						<CInputCustom
-							beforeIcon={<Lock color='#9CA3AF'/>}
-							labelName='비밀번호'
-							placeholder='비밀번호 입력'
-							afterIcon={<Eye color='#9CA3AF'/>}
+							beforeIcon={<Lock color="#9CA3AF" />}
+							labelName="비밀번호"
+							placeholder="비밀번호 입력"
+							afterIcon={<Eye color="#9CA3AF" />}
 							// afterIcon={<EyeOff />}
 						/>
-						
+
 						<CInputCustom
-							beforeIcon={<Lock color='#9CA3AF'/>}
-							labelName='비밀번호 확인'
-							placeholder='비밀번호 재입력'
-							afterIcon={<CircleCheck color="#5da1f8"/>}
+							beforeIcon={<Lock color="#9CA3AF" />}
+							labelName="비밀번호 확인"
+							placeholder="비밀번호 재입력"
+							afterIcon={<CircleCheck color="#5da1f8" />}
 							// afterIcon={<CircleCheck />}
 						/>
-					
+
 					</section>
-					
+
 					<section className={c.guideAndConsent}>
 						<div className={c.guideContentWrapper}>
-							<Info size={11} color="#9CA3AF"/>
+							<Info size={11} color="#9CA3AF" />
 							<span className={c.guideContent}>영문, 숫자, 특수문자 포함 8자리 이상</span>
 						</div>
-						
+
 						<div className={c.consentWrapper}>
 							<div className={c.checkConsent}>
-								<Checkbox checked={true}/>
+								<Checkbox checked={true} />
 								<span>이용약관 및 개인정보처리방침에 동의합니다</span>
 							</div>
 							<span className={c.consentClickButton}>내용 보기</span>
 						</div>
 					</section>
-					
+
 					<section className={c.joinButtonSection}>
 						<div className={c.joinButton}>
-							<UserPen size={18} color='#FFFFFF'/>
+							<UserPen size={18} color="#FFFFFF" />
 							<span>회원가입</span>
 						</div>
-						
+
 						<div className={c.loginButton}>
 							<p>
 								이미 계정이 있으신가요?
 								<span>로그인하기</span>
 							</p>
-						
+
 						</div>
 					</section>
 				</div>
 			</div>
-		)
-	}
-	
+		);
+	};
+
 	const renderLogin = () => {
 		return (
 			<div className={`${c.loginArea} ${c.stagger}`}>
@@ -157,92 +178,92 @@ export default function Home() {
 					<p>로그인</p>
 					<span>계정에 로그인하여 업무를 시작하세요</span>
 				</section>
-				
+
 				<section className={c.inputSection}>
 					<div className={c.inputItem}>
-						<Mail className={c.mail} color='#9CA3AF'/>
+						<Mail className={c.mail} color="#9CA3AF" />
 						<label htmlFor="email">이메일</label>
-						<input type="text" placeholder='이메일 주소를 입력하세요'
-									 onChange={(e) => setLoginUserInfo((prev) => ({...prev, email: e.target.value}))}/>
+						<input type="text" placeholder="이메일 주소를 입력하세요"
+									 onChange={(e) => setLoginUserInfo((prev) => ({ ...prev, email: e.target.value }))} />
 					</div>
-					
+
 					<div className={c.inputItem}>
-						<Lock className={c.mail} color='#9CA3AF'/>
+						<Lock className={c.mail} color="#9CA3AF" />
 						<label htmlFor="email">비밀번호</label>
 						<input
 							type="password"
-							placeholder='비밀번호를 입력하세요'
-							onChange={(e) => setLoginUserInfo((prev) => ({...prev, password: e.target.value}))}
+							placeholder="비밀번호를 입력하세요"
+							onChange={(e) => setLoginUserInfo((prev) => ({ ...prev, password: e.target.value }))}
 							onKeyDown={(e) => {
 								if (e.key === 'Enter') {
 									goLogin();
 								}
 							}}
-						
+
 						/>
-						<EyeOff className={c.eye} color='#9CA3AF'/>
+						<EyeOff className={c.eye} color="#9CA3AF" />
 						{/*<Eye className={c.mail} color='#9CA3AF'/>*/}
 					</div>
 				</section>
-				
+
 				<section className={c.loginOptionSection}>
 					<div className={c.loginOptionItem}>
-						<Checkbox/>
+						<Checkbox onClick={() => setIsSaveLogin(!isSaveLogin)} />
 						<span className={c.keepLoginTitle}>로그인 상태 유지</span>
 					</div>
-					
+
 					<div className={c.loginOptionItem}>
 						<span className={c.findPw}>비밀번호 찾기</span>
 					</div>
 				</section>
-				
+
 				<section className={c.loginButtonSection}>
 					<div className={c.loginButtonItem} onClick={() => goLogin()}>
-						<LogIn/>
+						<LogIn />
 						<span>로그인</span>
 					</div>
-					
+
 					<div className={c.loginButtonOr}>
 						<span>또는</span>
 					</div>
-					
+
 					<div>
-						<img src="kakaologin.png" alt="" className='w-[400px] h-[60px]'/>
+						<img src="kakaologin.png" alt="" className="w-[400px] h-[60px]" />
 					</div>
 				</section>
-				
+
 				<section className={c.joinSection}>
 					<div className={c.joinItem}>
 						<span className={c.noAccount}>계정이 없으신가요?</span>
 						<span className={c.applyJoin}>회원가입 신청</span>
 					</div>
-				
+
 				</section>
 			</div>
-		)
-	}
-	
+		);
+	};
+
 	return (
 		<div className={c.container}>
-			<Nav/>
-			
+			<Nav />
+
 			<div className={c.loginContentWrapper}>
 				<div className={`${c.guideArea} ${c.stagger}`}>
 					<p className={c.guideTitle}>
-						<ShieldCheck/>
+						<ShieldCheck />
 						<span>Enterprise HR Solution</span>
 					</p>
-					
+
 					<p className={c.guideCenterTitle}>
-						스마트한 인사관리의 <br/>
+						스마트한 인사관리의 <br />
 						<span className={c.centerTitleHL}>새로운 기준</span>
 					</p>
-					
+
 					<p className={c.guideDesc}>
-						직원 채용부터 급여, 근태까지 <br/>
+						직원 채용부터 급여, 근태까지 <br />
 						하나의 플랫폼으로 관리하세요
 					</p>
-					
+
 					<ul className={c.spec}>
 						<li>
 							<span className={c.specNumber}>2,400+</span>
@@ -257,36 +278,36 @@ export default function Home() {
 							<span className={c.specDesc}>서비스 운영</span>
 						</li>
 					</ul>
-					
+
 					<ul className={c.functionList}>
 						<li>
               <span className={c.functionIcon}>
-                <Users color='#60A5FA'/>
+                <Users color="#60A5FA" />
               </span>
-							
+
 							<div>
 								<p className={c.functionTitle}>인사관리</p>
 								<span className={c.functionDesc}>조직도, 인사발령, 직원 정보 통합 관리</span>
 							</div>
 						</li>
-						
+
 						<li>
               <span className={c.functionIcon}>
-                <BanknoteArrowUp color='#60A5FA'/>
+                <BanknoteArrowUp color="#60A5FA" />
               </span>
-							
-							
+
+
 							<div>
 								<p className={c.functionTitle}>급여관리</p>
 								<span className={c.functionDesc}>자동 급여 계산, 세금 신고, 명세서 발송</span>
 							</div>
 						</li>
-						
+
 						<li>
               <span className={c.functionIcon}>
-                <Clock color='#60A5FA'/>
+                <Clock color="#60A5FA" />
               </span>
-							
+
 							<div>
 								<p className={c.functionTitle}>근태관리</p>
 								<span className={c.functionDesc}>출퇴근, 휴가, 초과근무 실시간 모니터링</span>
@@ -294,11 +315,11 @@ export default function Home() {
 						</li>
 					</ul>
 				</div>
-				
+
 				{renderLogin()}
 				{/*{renderJoin()}*/}
-			
+
 			</div>
 		</div>
-	)
+	);
 }
